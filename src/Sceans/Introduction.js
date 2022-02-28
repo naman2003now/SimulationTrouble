f8259499695b7edfa7be8ec5e261111507dfbf7f
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-02-28 14:29:23 +0530

    I don't the spelling of contine

--- src/Sceans/Introduction.js
+++ src/Sceans/Introduction.js
@@ -4,7 +4,7 @@ import mainmenu from "./mainmenu.js"
 export default class Introduction {
 	constructor() {
 		this.story = [
-			"Hello, world!, Press enter to contine...",
+			"Hello, world!, Press enter to continue...",
 			"I am Naman Agrawal. A game developer!",
 			"I have been doing game development for more than 2 years. I am not going to bore you much with this non sense. Let's skip to the good part!",
 		]
